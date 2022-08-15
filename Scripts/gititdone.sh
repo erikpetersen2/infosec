e5b4a1b20bd12c5a0895fab033bd241441612302
Erik Petersen <erik@erikpetersen.tech>
2022-08-15 00:23:32 -0500

    Updates to gititdone.sh

--- Scripts/gititdone.sh
+++ Scripts/gititdone.sh
@@ -2,13 +2,13 @@
 
 ## THE I'M TOO LAZY TO RUN THE COMMANDS TO CLONE A GITHUB REPOSITORY SCRIPT ##
 
-##########################################################################################
-## Arguments									        ##
-## $1 = name of the local directory to be created				        ##
-## $2 = URL for cloning the repository via SSH					        ##
-## 										        ##
-## Example usage: ./gititdone.sh widget-code-repo git@github.com:johndoe/widgetcode.git ##
-##########################################################################################
+###########################################################################
+## Arguments								 ##
+## $1 = name of the local directory to be created			 ##
+## $2 = repository location (authorsname/repository title)               ##
+## 									 ##
+## Example usage: ./gititdone.sh widget-code-repo johndoe/widgetrepo     ##
+###########################################################################
 
 ## CREATE THE DIRECTORY TO HOUSE THE REPO
 mkdir ~/Documents/$1 2>/dev/null
@@ -21,4 +21,4 @@ git init
 
 ## CLONE THE REPOSITORY FROM GITHUB USING SSH 
 ##NOTE: THIS SCRIPT WILL ONLY WORK IF YOU HAVE ALREADY IMPORTED AN SSH KEY INTO GITHUB.
-git clone $2
+git clone git@github.com:$2
