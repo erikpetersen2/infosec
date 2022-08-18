1825f28b4dba430eec2e9aed90dc14f64325f85a
Erik Petersen <erik@erikpetersen.tech>
2022-08-18 00:06:27 -0500

    Added Environmental-Config directory in Scripts

---
+++ Scripts/Environment-Config/find-demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Find Command Demo For Cyber Patriot
+
+touch /tmp/flag.txt
+echo "A flag named flag.txt has been placed on your system... can you find it?"
+echo "You found the flag!" > /tmp/flag.txt
