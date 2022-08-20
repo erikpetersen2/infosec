e0eb141310b57ecbc859126c970cafbc7e7add13
Erik Petersen <erik@erikpetersen.tech>
2022-08-20 15:05:57 -0500

    Amended scripts for clarity

--- Scripts/Environment-Config/find-demo.sh
+++ Scripts/Environment-Config/find-demo.sh
@@ -2,9 +2,11 @@
 
 ## Find Command Demo For Cyber Patriot
 
-touch /tmp/flag.txt
+touch /tmp/tmp
 echo ""
 echo "Time to practice your skills with the find command!"
 echo ""
 echo "A flag named flag.txt has been placed on your system... can you find it?"
-echo "You found the flag!" > /tmp/flag.txt
+echo "You found the flag!"  > /tmp/tmp
+base64 /tmp/tmp > /tmp/flag.txt
+rm /tmp/tmp
